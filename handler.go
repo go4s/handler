6dff5aba482c2bf30d3d1ba425ca3a0f68acf63b
li.zhang <294638057@qq.com>
2019-12-15 13:56:27 +0800

    Update handler.go

--- handler.go
+++ handler.go
@@ -2,13 +2,13 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
-type Register func(*gin.IRouter) error
+type Register func(gin.IRouter) error
 
 var (
 	registers = [] Register{}
 )
 
-func Hook(engine *gin.IRouter) {
+func Hook(engine gin.IRouter) {
 	var err error
 	for _, register := range registers {
 		err = register(engine)
