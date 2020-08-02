8f5a5990c383cb64e692cf042e9e445792b844ee
DrZhang <294638057@qq.com>
2020-08-02 11:09:00 +0800

    Changes to be committed: 	modified:   handler.go

--- handler.go
+++ handler.go
@@ -1,23 +1,76 @@
 package handler
 
-import "github.com/gin-gonic/gin"
-
-type Register func(gin.IRouter) error
-
-var (
-	registers = [] Register{}
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"sync"
 )
 
+type Register func(gin.IRouter)
+
+var (
+	once      = new(sync.Once)
+	registers []Register
+)
+
+func init() {
+	once.Do(func() {
+		registers = make([]Register, 0)
+	})
+}
+
 func Hook(engine gin.IRouter) {
-	var err error
 	for _, register := range registers {
-		err = register(engine)
-		if err != nil {
-			panic(err)
-		}
+		register(engine)
 	}
 }
 
-func Add(r Register) {
-	registers = append(registers, r)
+func Add(r interface {
+	Version
+	Group
+}) {
+	registers = append(registers, func(g gin.IRouter) {
+		api := g.Group(fmt.Sprintf("api/%s/%s", r.Version(), r.Group()))
+		{
+			// standard
+			if handle, found := r.(Watchable); found {
+				api.GET("/events/*id", handle.Watch)
+			}
+			if handle, found := r.(Listable); found {
+				api.GET("/*id", handle.List)
+			}
+			if handle, found := r.(Creatable); found {
+				api.POST("/*id", handle.Create)
+			}
+			if handle, found := r.(Deletable); found {
+				api.DELETE("/*id", handle.Delete)
+			}
+			// use defiled
+			if handle, found := r.(Customize); found {
+				handle.Raw(g)
+			}
+		}
+	})
+}
+
+type Version interface {
+	Version() string
+}
+type Group interface {
+	Group() string
+}
+type Watchable interface {
+	Watch(*gin.Context)
+}
+type Listable interface {
+	List(*gin.Context)
+}
+type Creatable interface {
+	Create(*gin.Context)
+}
+type Deletable interface {
+	Delete(*gin.Context)
+}
+type Customize interface {
+	Raw(router gin.IRouter)
 }
